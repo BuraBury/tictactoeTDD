666ea8d29bbeeacbb68eef93a113e82f0960e66a
Paulina <bura.bury@gmail.com>
2020-06-21 13:51:35 +0200

    21.06

--- src/main/java/org/sda/tictactoe/SDA/TicTacToe.java
+++ src/main/java/org/sda/tictactoe/SDA/TicTacToe.java
@@ -1,4 +1,13 @@
 package org.sda.tictactoe.SDA;
 
 public class TicTacToe {
+
+    public void play(int X, int Y) {
+        if (X < 1 || X > 3) {
+            throw new RuntimeException("X is outside board");
+        }
+        if (Y < 1 || Y > 3) {
+            throw new RuntimeException("Y is outside board");
+        }
+    }
 }
--- src/test/java/org/sda/tictactoe/SDA/TicTacToeTest.java
+++ src/test/java/org/sda/tictactoe/SDA/TicTacToeTest.java
@@ -1,6 +1,32 @@
 package org.sda.tictactoe.SDA;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TicTacToeTest {
+class TicTacToeTest {
+    private TicTacToe ticTacToe;
+
+    @BeforeEach
+    public void SetUp() {
+        ticTacToe = new TicTacToe();
+    }
+
+    @Test
+    public void whenXIsOutside() {
+        TicTacToe ticTacToe = new TicTacToe();
+        assertThatThrownBy(() -> ticTacToe.play(5, 3))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void whenYIsOutside() {
+        TicTacToe ticTacToe = new TicTacToe();
+        assertThatThrownBy(() -> ticTacToe.play(5, 3))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+
 }
